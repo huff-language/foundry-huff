b781a7aec437da745b61d56f97632ae88d08ca02
JetJadeja <jetjadeja@gmail.com>
2022-06-22 15:39:25 -0700

    Change minimum compiler version to 0.8.13

--- src/HuffDeployer.sol
+++ src/HuffDeployer.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.8.13 <0.9.0;
+pragma solidity >=0.8.13 <0.9.0; 
 
 ///@notice This cheat codes interface is named _CheatCodes so you can use the CheatCodes interface in other testing files without errors
 interface _CheatCodes {
