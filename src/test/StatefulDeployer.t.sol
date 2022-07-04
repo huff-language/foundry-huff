845f822f87df29bb9609315272d881c0c6739813
Andreas Bigger <abigger87@gmail.com>
2022-07-04 13:44:36 -0400

    :rocket:

--- src/test/StatefulDeployer.t.sol
+++ src/test/StatefulDeployer.t.sol
@@ -9,7 +9,7 @@ import {StatefulDeployer} from "../StatefulDeployer.sol";
 
 contract StatefulDeployerTest is Test {
   StatefulDeployer public deployer;
-  INumber public number;
+  IConstructor public construct;
 
   function setUp() public {
     deployer = new StatefulDeployer();
@@ -26,45 +26,32 @@ contract StatefulDeployerTest is Test {
   }
 
   function testDeployWithArgsAndCode() public {
-    // Create Constructor using Chaining
-    // chained = Constructor(HuffDeployer.with_args(bytes.concat(first_arg, abi.encode(uint256(0x420)))).deploy("test/contracts/Constructor"));
+    deployer.setArgs(bytes.concat(abi.encode(uint256(0x420)), abi.encode(uint256(0x420))));
+    deployer.setCode(""
+      "#define macro CONSTRUCTOR() = takes(0) returns (0) { \n"
+      "    // Copy the first argument into memory \n"
+      "    0x20                        // [size] - byte size to copy \n"
+      "    0x40 codesize sub           // [offset, size] - offset in the code to copy from \n"
+      "    0x00                        // [mem, offset, size] - offset in memory to copy to \n"
+      "    codecopy                    // [] \n"
+      "    // Store the first argument in storage \n"
+      "    0x00 mload                  // [arg] \n"
+      "    [CONSTRUCTOR_ARG_ONE]       // [CONSTRUCTOR_ARG_ONE, arg] \n"
+      "    sstore                      // [] \n"
+      "    // Copy the second argument into memory \n"
+      "    0x20                        // [size] - byte size to copy \n"
+      "    0x20 codesize sub           // [offset, size] - offset in the code to copy from \n"
+      "    0x00                        // [mem, offset, size] - offset in memory to copy to \n"
+      "    codecopy                    // [] \n"
+      "    // Store the second argument in storage \n"
+      "    0x00 mload                  // [arg] \n"
+      "    [CONSTRUCTOR_ARG_TWO]       // [CONSTRUCTOR_ARG_TWO, arg] \n"
+      "    sstore                      // [] \n"
+      "}"
+    );
 
-    // // Create Constructor with code and args
-    // no_constructor = Constructor(
-    //     HuffDeployer
-    //     .with_args(bytes.concat(first_arg, abi.encode(uint256(0x420))))
-    //     .with_code("",
-    //         "#define macro CONSTRUCTOR() = takes(0) returns (0) {",
-    //         "    // Copy the first argument into memory",
-    //         "    0x20                        // [size] - byte size to copy",
-    //         "    0x40 codesize sub           // [offset, size] - offset in the code to copy from",
-    //         "    0x00                        // [mem, offset, size] - offset in memory to copy to",
-    //         "    codecopy                    // []",
-    //         "    // Store the first argument in storage",
-    //         "    0x00 mload                  // [arg]",
-    //         "    [CONSTRUCTOR_ARG_ONE]       // [CONSTRUCTOR_ARG_ONE, arg]",
-    //         "    sstore                      // []",
-    //         "    // Copy the second argument into memory",
-    //         "    0x20                        // [size] - byte size to copy",
-    //         "    0x20 codesize sub           // [offset, size] - offset in the code to copy from",
-    //         "    0x00                        // [mem, offset, size] - offset in memory to copy to",
-    //         "    codecopy                    // []",
-    //         "    // Store the second argument in storage",
-    //         "    0x00 mload                  // [arg]",
-    //         "    [CONSTRUCTOR_ARG_TWO]       // [CONSTRUCTOR_ARG_TWO, arg]",
-    //         "    sstore                      // []",
-    //         "}"
-    //     )
-    //     .deploy()
-    // );
-
-    string memory constructor_macro = "#define macro CONSTRUCTOR() = takes(0) returns (0) {}";
-
-    deployer.setCode(constructor_macro);
-
-    number = INumber(deployer.deploy("test/contracts/Number"));
-    assertEq(number.getNumber(), 0);
-    number.setNumber(42);
-    assertEq(number.getNumber(), 42);
+    construct = IConstructor(deployer.deploy("test/contracts/NoConstructor"));
+    assertEq(address(0x420), construct.getArgOne());
+    assertEq(uint256(0x420), construct.getArgTwo());
   }
 }
