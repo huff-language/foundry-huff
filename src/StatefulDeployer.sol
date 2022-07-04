a8a91a090d86e111e3c578f573fe4094baf79621
Andreas Bigger <abigger87@gmail.com>
2022-07-04 10:07:09 -0400

    module removal

--- src/StatefulDeployer.sol
+++ src/StatefulDeployer.sol
@@ -1,9 +1,12 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity >=0.8.13 <0.9.0;
 
+import {strings} from "stringutils";
 import {HuffDeployer} from "./HuffDeployer.sol";
 
 contract StatefulDeployer {
+  using strings for *;
+
   /// @notice additional code to append to the source file
   string public code;
 
@@ -22,14 +25,20 @@ contract StatefulDeployer {
 
   /// @notice Deployment wrapper
   function deploy(string memory file) public returns (address) {
-    // Create the temp file
-    string memory tempFile = string(
-      string.concat(
-        "src/__TEMP__",
-        file,
-        ".huff"
-      )
-    );
+    // Split the file into it's parts
+    strings.slice memory s = file.toSlice();
+    strings.slice memory delim = "/".toSlice();
+    string[] memory parts = new string[](s.count(delim));
+    for (uint i = 0; i < parts.length; i++) {
+      parts[i] = s.split(delim).toString();
+    }
+
+    // Re-concatenate the file with a "__TEMP__" prefix
+    string memory tempFile = parts[i];
+    for (uint i = 1; i < parts.length - 1; i++) {
+      tempFile = string.concat(tempFile, "/", parts[i]);
+    }
+    tempFile = string.concat(tempFile, "/", "__TEMP__", parts[parts.length - 1]);
 
     // Paste the code in a new temp file
     string[] memory create_cmds = new string[](6);
@@ -40,6 +49,8 @@ contract StatefulDeployer {
     create_cmds[4] = ">";
     create_cmds[5] = tempFile;
     HuffDeployer.vm.ffi(create_cmds);
+
+    // echo "" | cat > src/__TEMP__test/contracts/Number.huff
 
     // Append the real code to the temp file
     string[] memory append_cmds = new string[](4);
