8f1fb69ac91312ad23b15a7ab55c420d820d4eed
losha <83345377+0xlosha@users.noreply.github.com>
2023-02-23 03:04:30 +0100

    Update binary_check.sh

--- scripts/binary_check.sh
+++ scripts/binary_check.sh
@@ -1,9 +1,17 @@
 #! /bin/bash
 
-if ! [[ "$(npm list -g huffc)" =~ "empty" ]]; then
+is_installed_npm() {
+  command -v npm &> /dev/null
+}
+
+is_installed_yarn() {
+  command -v yarn &> /dev/null
+}
+
+if is_installed_npm && ! [[ "$(npm list -g huffc)" =~ "empty" ]]; then
   # huffc was installed via npm, return 0x00
   echo -n 0x00
-elif [[ "$(yarn global list)" =~ "huffc" ]]; then
+elif is_installed_yarn && [[ "$(yarn global list)" =~ "huffc" ]]; then
   # huffc was installed via yarn, return 0x00
   echo -n 0x00
 else
