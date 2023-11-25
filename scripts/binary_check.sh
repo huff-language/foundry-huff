d03ff4dd0749871094901949429874265f8d1908
pawurb <contact@pawelurbanek.com>
2023-11-25 21:44:23 +0100

    Silence binary check errors

--- scripts/binary_check.sh
+++ scripts/binary_check.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
-if ! [[ "$(npm list -g huffc)" =~ "empty" ]]; then
+if ! [[ "$(npm list -g huffc 2> /dev/null)" =~ "empty" ]]; then
   # huffc was installed via npm, return 0x00
   echo -n 0x00
-elif [[ "$(yarn global list)" =~ "huffc" ]]; then
+elif [[ "$(yarn global list 2> /dev/null)" =~ "huffc" ]]; then
   # huffc was installed via yarn, return 0x00
   echo -n 0x00
 else
