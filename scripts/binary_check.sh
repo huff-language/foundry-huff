a8abf3e8fbb42aae670065eadb3b2063be82fdd0
Maddiaa0 <47148561+Maddiaa0@users.noreply.github.com>
2024-03-20 15:21:05 +0000

    fix: also npm / yarn pipe stderr to null

--- scripts/binary_check.sh
+++ scripts/binary_check.sh
@@ -2,7 +2,7 @@
 
 # Check if npm is installed
 if command -v npm &> /dev/null; then
-  if ! [[ "$(npm list -g huffc)" =~ "empty" ]]; then
+  if ! [[ "$(npm list -g huffc 2> /dev/null)" =~ "empty" ]]; then
     # huffc was installed via npm, return 0x00
     echo -n 0x00
     exit 0
@@ -11,7 +11,7 @@ fi
 
 # Check if yarn is installed
 if command -v yarn &> /dev/null; then
-  if [[ "$(yarn global list)" =~ "huffc" ]]; then
+  if [[ "$(yarn global list 2> /dev/null)" =~ "huffc" ]]; then
     # huffc was installed via yarn, return 0x00
     echo -n 0x00
     exit 0
